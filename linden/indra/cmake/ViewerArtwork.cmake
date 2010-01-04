1f62d1bdd8057aa1183b76c80e9e6d11ec94e478
McCabe Maxsted <hakushakukun@gmail.com>
2010-01-04 14:51:34 +0800

    Added three new skins: meerkat, gemini, and dark

--- linden/indra/cmake/ViewerArtwork.cmake
+++ linden/indra/cmake/ViewerArtwork.cmake
@@ -1,14 +1,20 @@
 # -*- cmake -*-
 #
-# Download viewer artwork even when using standalone
+# Download viewer artwork and skins even when using standalone
 
 include(Variables)
 include(Prebuilt)
 
 if (NOT STANDALONE)
   use_prebuilt_binary(artwork)
+  use_prebuilt_binary(skins-dark)
+  use_prebuilt_binary(skins-gemini)
+  use_prebuilt_binary(skins-meerkat)
 else (NOT STANDALONE)
   set(STANDALONE OFF)
     use_prebuilt_binary(artwork)
+    use_prebuilt_binary(skins-dark)
+	use_prebuilt_binary(skins-gemini)
+	use_prebuilt_binary(skins-meerkat)
   set(STANDALONE ON)
 endif (NOT STANDALONE)
