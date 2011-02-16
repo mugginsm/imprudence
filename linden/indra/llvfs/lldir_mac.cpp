12fd835ef7ef25447f43c9cb783ebe13c884d8c8
Patrick Sapinski <patrick@sapinski.com>
2011-02-16 14:45:00 -0500

    another fix for mac directory names

--- linden/indra/llvfs/lldir_mac.cpp
+++ linden/indra/llvfs/lldir_mac.cpp
@@ -122,7 +122,7 @@ LLDir_Mac::LLDir_Mac()
 	CFStringRef		stringRef = NULL;
 	OSStatus		error = noErr;
 	FSRef			fileRef;
-	CFStringRef		secondLifeString = CFSTR("astra");
+	CFStringRef		secondLifeString = CFSTR("Astra Viewer");
 	
 	mainBundleRef = CFBundleGetMainBundle();
 		
@@ -173,7 +173,7 @@ LLDir_Mac::LLDir_Mac()
 		if (error == noErr)
 		{
 			FSRefToLLString(&cacheDirRef, mOSCacheDir);
-			(void)CFCreateDirectory(&cacheDirRef, CFSTR("astra"),NULL);
+			(void)CFCreateDirectory(&cacheDirRef, CFSTR("Astra Viewer"),NULL);
 		}
 		
 		// mOSUserAppDir
